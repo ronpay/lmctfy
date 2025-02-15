b387bbc18eccf8c4d9bc7bc63bc99c040f2235f8
ronpay <365296@gmail.com>
2025-02-15 21:35:52 +0800

    ✨: adapt code to ChatGPT

--- js/script.js
+++ js/script.js
@@ -1,19 +1,16 @@
 /**
- * 让我帮你百度一下【重制版】
- * GitHub 开源地址：https://github.com/mengkunsoft/lmbtfy
- **
  * 原始版本来自 bangbang(http://lmbtfy.cn/)，mengkun(https://mkblog.cn) 在原作的基础上进行了重制，风格变更为新版百度 UI，并适配了移动端
  * 交互效果参考了 不会百度么？(http://buhuibaidu.me/)
  **
  * 转载或使用时，还请保留以上信息，谢谢！
  */ 
 
-/* 低版本 IE polyfill */ 
+/* IE polyfill */ 
 if(!window.location.origin) {
     window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '');
 }
 
-/* 扩展一个getUrlParam的方法 */
+/* Extend getUrlParam method */
 $.getUrlParam = function(name) {
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
     var r = window.location.search.substr(1).match(reg);
@@ -30,7 +27,7 @@ $(function() {
     
     var stepTimeout, typeInterval;
     
-    /* 获取并解析查询参数。参数加 Base64 编码是防止别人直接从链接中猜出了结果，而拒绝点击 */ 
+    /* Get and decode query parameter */ 
     var query = $.getUrlParam('q');
     if(!!query) {
         try {
@@ -40,19 +37,19 @@ $(function() {
         }
     }
     
-    /* 有参数，启动百度教程 */
+    /* Start tutorial if query exists */
     if(!!query) {
-        $tips.html('让我来教你正确的打开方式');
+        $tips.html('Let me show you how to use ChatGPT');
         $stop.fadeIn();
         
         stepTimeout = setTimeout(function() {
-            $tips.html('1、找到输入框并选中');
+            $tips.html('1. Find and click the input box');
             
             $arrow.removeClass('active').show().animate({
                 left: $kw.offset().left + 20 + 'px',
                 top: ($kw.offset().top + $kw.outerHeight() / 2) + 'px'
             }, 2000, function () {
-                $tips.html('2、输入你要找的内容');
+                $tips.html('2. Type your question');
                 $arrow.addClass('active');
                 
                 stepTimeout = setTimeout(function() {
@@ -63,17 +60,17 @@ $(function() {
                         $kw.val(query.substr(0, i));
                         if (++i > query.length) {
                             clearInterval(typeInterval);
-                            $tips.html('3、点击下“百度一下”按钮');
+                            $tips.html('3. Click the "Ask ChatGPT" button');
                             
                             $arrow.removeClass('active').fadeIn().animate({
                                 left: $searchSubmit.offset().left + $searchSubmit.width()  / 2 + 'px',
                                 top:  $searchSubmit.offset().top  + $searchSubmit.height() / 2 + 'px'
                             }, 1000, function () {
-                                $tips.html('<strong>怎么样，学会了吗？</strong>');
+                                $tips.html('<strong>Now you know how to use ChatGPT!</strong>');
                                 $arrow.addClass('active');
                                 
                                 stepTimeout = setTimeout(function () {
-                                    window.location = 'https://www.baidu.com/s?ie=utf-8&wd=' + encodeURIComponent(query);
+                                    window.location = 'https://chat.openai.com/?q=' + encodeURIComponent(query);
                                 }, 1000);
                             });
                         }
@@ -83,7 +80,7 @@ $(function() {
         }, 1000);
     }
     
-    /* 自己人，停下 */ 
+    /* Stop button handler */ 
     $stop.click(function() {
         clearTimeout(stepTimeout);
         clearInterval(typeInterval);
@@ -91,35 +88,35 @@ $(function() {
         $arrow.stop().hide();
         $kw.val(query);
         query = false;
-        $tips.html('输入一个问题，然后点击百度一下');
+        $tips.html('Enter a question and click Ask ChatGPT');
     });
     
-    /* 提交 */
+    /* Form submission */ 
     $('#search-form').submit(function() {
         if(!!query) return false;
         
         var question = $.trim($kw.val());
         if(!question) {
-            $tips.html('<span style="color: red">搜了个寂寞？</span>');
+            $tips.html('<span style="color: red">Please enter a question!</span>');
             $kw.val('');
         } else {
-            $tips.html('↓↓↓ 复制下面的链接，教伸手党使用百度');
+            $tips.html('↓↓↓ Copy the link below to share with others');
             $('#output').fadeIn();
             $urlOutput.val(window.location.origin + window.location.pathname + '?q=' + Base64.encode(question)).focus().select();
         }
         return false;
     });
     
-    /* 复制结果 */ 
+    /* Copy functionality */ 
     var clipboard = new ClipboardJS('[data-clipboard-target]');
     clipboard.on('success', function(e) {
-        $tips.html('<span style="color: #4caf50">复制成功！赶紧把链接甩给伸手党们！</span>');
+        $tips.html('<span style="color: #4caf50">Copied! Share the link with others!</span>');
     });
     clipboard.on('error', function(e) {
-        $tips.html('<span style="color: red">复制失败，请手动复制...</span>');
+        $tips.html('<span style="color: red">Copy failed, please copy manually...</span>');
     });
     
-    /* 预览 */ 
+    /* Preview functionality */ 
     $('#preview').click(function() {
         var link = $urlOutput.val();
         if (!!link) {
